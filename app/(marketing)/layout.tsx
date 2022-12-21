bfb5986a76143c36ff1504e1f51e28f89a0e0ac9
Hemant Negi <nhemnt@gmail.com>
2022-12-22 00:15:12 +0530

    update: hide login and routes

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -17,12 +17,12 @@ export default async function MarketingLayout({
         <div className="flex h-16 items-center justify-between border-b border-b-slate-200 py-4">
           <MainNav items={marketingConfig.mainNav} />
           <nav>
-            <Link
+            {false && <Link
               href="/login"
               className="relative inline-flex h-8 items-center rounded-md border border-transparent bg-brand-500 px-6 py-1 text-sm font-medium text-white hover:bg-brand-400 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
             >
               Login
-            </Link>
+            </Link>}
           </nav>
         </div>
       </header>
