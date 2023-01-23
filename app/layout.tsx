a3fef96b43ad121531a4669689df4b1e908b8698
Hemant Negi <nhemnt@gmail.com>
2023-01-23 21:01:22 +0530

    feat: add gadd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import { Inter as FontSans } from "@next/font/google"
-
+import Script from "next/script"
 import "@/styles/globals.css"
 
 import { cn } from "@/lib/utils"
@@ -27,6 +27,13 @@ export default function RootLayout({ children }: RootLayoutProps) {
       )}
     >
       <head />
+      <Script
+        id="Adsense-id"
+        crossOrigin="anonymous"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2049525361854422"
+      />
       <body className="min-h-screen">
         {children}
         <Analytics />
